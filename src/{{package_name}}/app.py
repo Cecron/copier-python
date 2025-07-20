90d615b6490401d037028729d68945d65a8a3e99
Cecron <b9kvz7be6v@snkmail.com>
2025-07-20 20:53:53 +0200

    Add option to get skeleton for sub-commands
    - Added Todo list
    - feat: Add option to get skeleton for sub-commands
    - fix: Package name is not allowed to have underscores
    - fix: Running the skeleton using "python -m package" now works
    - CHANGE: Removed question about initializing git since copier needs a
        git repo in order to do "copier update".
    - CHANGE: Changed backend  from uv to Setuptools. Will add choice
        later.

--- src/{{package_name}}/app.py
+++ src/{{package_name}}/app.py
@@ -1,4 +1,7 @@
 
 
-def main():
-    print("Hello world")
+def main(args):
+    print(f"Invoked with {args}")
+    if args.action is not None:
+        args.action(args)
+
---
+++ src/{{package_name}}/{% if use_subcommands %}commands{% endif %}/hello.py
@@ -0,0 +1,14 @@
+class Hello:
+    def __init__(self):
+        self.parser = None
+
+    def add_subparser(self, subparsers):
+        self.parser = subparsers.add_parser('hello', help="Great someone by name.")
+        self.parser.add_argument(
+            '-n', '--name', metavar="NAME", required=False, type=str, dest='name', default="World",
+            help="Name to greet. (default: %(default)s)"
+        )
+        self.parser.set_defaults(action=self)
+
+    def __call__(self, args):
+        print(f"Hello {args.name}!")

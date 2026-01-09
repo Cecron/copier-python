fd08985943cb597fefdd4893878b6ba386943d6f
Cecron <b9kvz7be6v@snkmail.com>
2026-01-09 15:47:41 +0100

    Add typing target in justfile for type checking

--- template/src/{{package_name}}/__init__.py
+++ template/src/{{package_name}}/__init__.py
@@ -1,3 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__)
+if __package__ is None:
+    __version__ = "0.0.0"
+else:
+    __version__ = importlib.metadata.version(__package__)
